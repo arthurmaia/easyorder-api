6482a8ed92ca25d20e8813ff581fa3a780a20327
Arthur Maia <arthur.maia.dev@gmail.com>
2023-01-22 19:42:05 -0300

    voltei valores defaults

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,27 +1,27 @@
-// export enum OrderStatusEnum {
-// 	APPROVED = 1,
-// 	PENDING = 2,
-// 	CANCELED = 3,
-// }
+export enum OrderStatusEnum {
+	APPROVED = 1,
+	PENDING = 2,
+	CANCELED = 3,
+}
 
-// export const OrderStatusDescriptions = {
-// 	[OrderStatusEnum.APPROVED]: 'Aprovado',
-// 	[OrderStatusEnum.PENDING]: 'Em Andamento',
-// 	[OrderStatusEnum.CANCELED]: 'Cancelado',
-// };
+export const OrderStatusDescriptions = {
+	[OrderStatusEnum.APPROVED]: 'Aprovado',
+	[OrderStatusEnum.PENDING]: 'Em Andamento',
+	[OrderStatusEnum.CANCELED]: 'Cancelado',
+};
 
-// export enum CategoryEnum {
-// 	LUNCHES = 1,
-// 	SAVORY = 2,
-// 	DRINKS = 3,
-// 	DESSERTS = 4,
-// }
+export enum CategoryEnum {
+	LUNCHES = 1,
+	SAVORY = 2,
+	DRINKS = 3,
+	DESSERTS = 4,
+}
 
-// export const CategoryDescriptions = {
-// 	[CategoryEnum.LUNCHES]: 'Lanches',
-// 	[CategoryEnum.SAVORY]: 'Salgados',
-// 	[CategoryEnum.DRINKS]: 'Bebidas',
-// 	[CategoryEnum.DESSERTS]: 'Sobremesas',
-// };
+export const CategoryDescriptions = {
+	[CategoryEnum.LUNCHES]: 'Lanches',
+	[CategoryEnum.SAVORY]: 'Salgados',
+	[CategoryEnum.DRINKS]: 'Bebidas',
+	[CategoryEnum.DESSERTS]: 'Sobremesas',
+};
 
 export const defaultProductImageUrl = 'https://i.imgur.com/pDWhieg.jpg';
