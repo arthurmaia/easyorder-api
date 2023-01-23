964bdda8b1cd2f4a2289be71b34a6924cccad6b7
Arthur Maia <arthur.maia.dev@gmail.com>
2023-01-23 20:40:25 -0300

    removido tipagem desnecessaria

--- src/bill/bill.controller.ts
+++ src/bill/bill.controller.ts
@@ -1,10 +1,8 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { PublicOrderDto } from 'src/order/dto/public-order.dto';
 
 import { BillService } from './bill.service';
 import { CreateBillDto } from './dto/create-bill.dto';
-import { PublicBillDto } from './dto/public-bill.dto';
 
 @ApiTags('bill')
 @Controller('bill')
@@ -12,19 +10,17 @@ export class BillController {
 	constructor(private readonly billService: BillService) {}
 
 	@Post('create')
-	async createBill(
-		@Body() { barTableId }: CreateBillDto
-	): Promise<PublicBillDto> {
+	async createBill(@Body() { barTableId }: CreateBillDto) {
 		return await this.billService.createBill(barTableId);
 	}
 
 	@Get('all')
-	async getAllBills(): Promise<PublicBillDto[]> {
+	async getAllBills() {
 		return await this.billService.getAllBills();
 	}
 
 	@Get('getOrdersByBillId/:id')
-	async getOrderByBillId(@Param('id') id: string): Promise<PublicOrderDto[]> {
+	async getOrderByBillId(@Param('id') id: string) {
 		return await this.billService.getOrderByBillId(id);
 	}
 }
